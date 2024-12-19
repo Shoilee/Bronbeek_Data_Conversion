64e65e818f4c601e780a0ee504c098515ba02140
Shoilee <sarahshoilee121@gmail.com>
2024-12-19 16:32:59 +0100

    startup cliopatria

--- local.pl
+++ local.pl
@@ -1,1 +1,10 @@
+:- use_module(library(semweb/rdf_library)).
 :- set_prolog_flag(stack_limit, 2_147_483_648) .
+
+load_bronbeek :-
+    rdf_attach_library('RDF/void.ttl'),
+    rdf_library:rdf_load_library('bronbeek-enriched'),
+    rdf_library:rdf_load_library('bronbeek').
+
+:- load_bronbeek.
+

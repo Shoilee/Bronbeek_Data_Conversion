be3dc7e3a42cefdf3f17075c4a37fbce01e59a21
Shoilee <sarahshoilee121@gmail.com>
2024-12-19 18:09:40 +0100

    commnet out loading bronbeek each time

--- local.pl
+++ local.pl
@@ -1,10 +1,13 @@
 :- use_module(library(semweb/rdf_library)).
+:- use_module(library(semweb/rdf_db)).
+
 :- set_prolog_flag(stack_limit, 2_147_483_648) .
 
 load_bronbeek :-
     rdf_attach_library('RDF/void.ttl'),
+    rdf_load('RDF/void.ttl', [graph('https://pm.labs.vu.nl/void')]),
     rdf_library:rdf_load_library('bronbeek-enriched'),
     rdf_library:rdf_load_library('bronbeek').
 
-:- load_bronbeek.
+% :- load_bronbeek.
 
